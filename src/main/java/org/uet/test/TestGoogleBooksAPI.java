84be4aa6715593bd792a344248b30c5f7867e5c3
vanthao43st <nvthao43.hnvn@gmail.com>
2024-12-04 22:33:21 +0700

    Update TestGoogleBooksApi

--- src/main/java/org/uet/test/TestGoogleBooksAPI.java
+++ src/main/java/org/uet/test/TestGoogleBooksAPI.java
@@ -6,8 +6,6 @@ import org.uet.entity.Document;
 import java.util.ArrayList;
 
 public class TestGoogleBooksAPI {
-//    private static final GoogleBooksAPI googleBooksAPI = new GoogleBooksAPI();
-
     private static void searchDocumentByISBN() {
         ArrayList<Document> documents = GoogleBooksAPI.searchBookByISBN("9780134685991");
         for (Document document : documents) {
